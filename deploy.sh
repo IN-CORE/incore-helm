6d2a9d892a6f919b342bbf2d7fbc69dbd472a957
Rob Kooper <kooper@illinois.edu>
2023-03-02 15:56:43 -0600

    simple deply script

--- deploy.sh
+++ deploy.sh
@@ -4,23 +4,14 @@
 set -e
 
 if [ $# == 0 ]; then
-  CLUSTER="tst"
+  CLUSTER="dev"
 else
   CLUSTER="$1"
 fi
 
 
 # switch to correct cluster
-kubectx incore-${CLUSTER}
-
-# make sure incore namespace exists
-kubectl create namespace incore || true
-
-# load secret to pull images
-kubectl -n incore create secret generic regcred --from-file=.dockerconfigjson=regcred.json --type=kubernetes.io/dockerconfigjson || true
-
-# create PVC
-#sed "s/-tst/-${CLUSTER}/g" incore-pvc.yaml | kubectl -n incore apply -f -
+kubectl config use-context incore-${CLUSTER}
 
 # deploy
-helm upgrade --install --namespace incore --create-namespace incore incore  --values values-incore-${CLUSTER}.yaml
+helm upgrade --namespace incore incore . --values values-incore-${CLUSTER}.yaml
